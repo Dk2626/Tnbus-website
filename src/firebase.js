d64e8493a7347f16375b99355978f6f8c9b18214
Dk2626 <dineshkumardssd@gmail.com>
2022-08-11 23:03:00 +0530

    sdf

--- src/firebase.js
+++ src/firebase.js
@@ -23,15 +23,12 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCCZ2bo_iPbtvarsADQe84qX2s9cWPMq3U',
-  authDomain: 'touronapp-248e4.firebaseapp.com',
-  databaseURL: 'https://touronapp-248e4.firebaseio.com',
-  // databaseURL: "https://touronapp-248e4-3793a.firebaseio.com/",
-  projectId: 'touronapp-248e4',
-  storageBucket: 'touronapp-248e4.appspot.com',
-  messagingSenderId: '813320271971',
-  appId: '1:813320271971:web:5a10483e3c11bc953aa056',
-  measurementId: 'G-KCPSW6WFC9',
+  apiKey: 'AIzaSyAMgy9cc5xciNxG9BMyQG4b718iI1-bQ9Y',
+  authDomain: 'travelon-22f65.firebaseapp.com',
+  projectId: 'travelon-22f65',
+  storageBucket: 'travelon-22f65.appspot.com',
+  messagingSenderId: '367671707283',
+  appId: '1:367671707283:web:19614f911281939bacef66',
 };
 
 let fire;
